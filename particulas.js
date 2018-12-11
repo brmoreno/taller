3e50a466df41ab454be02b5c48dde119dca2d60c
Benjamín R. Moreno Ortiz <benjamin_moreno_ortiz@alumni.brown.edu>
2018-12-11 11:27:45 -0600

    Update particulas.js

--- particulas.js
+++ particulas.js
@@ -1,10 +1,14 @@
 var particulas = [];
+var nodos = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255);
 	for (i = 0; i <200; i++){
 		c = color(random(255),random(255),random(255));
 		particulas.push(new Particula(random(width),random(height),random(-1,1),random(-1,1),random(10,50),c));
+	}
+	for (i = 0; i<10; i++){
+		nodos.push(new Nodo(this.random(width), this.random(height),random(50,200)));
 	}
 }
 
@@ -23,11 +27,20 @@ function Particula(x_, y_, dx_, dy_,t_,c_){
 	this.dy = dy_;
 	this.t = t_;
 	this.c = c_;
+	this.escala = 0.1;
 	
 	this.display = function (){
 		noStroke();
 		fill(this.c);
 		ellipse(this.x,this.y,this.t,this.t);
+	}
+	
+	this.cEscala = function (anodo_[]){
+		for(i = 0; i<anodo_.length; i++){
+			n = anodo_[i];
+			distancia = dist(this.x,this.y,n.x,n.y);
+			this.escala = map(distancia,0,this.r,0.1,1);
+		}
 	}
 	
 	this.mover = function(){
@@ -40,4 +53,10 @@ function Particula(x_, y_, dx_, dy_,t_,c_){
 			this.dy *= -1;
 		}
 	}	
+}
+
+function Nodo (x_, y_ , r_){
+	this.x = x_;
+	this.y = y_;
+	this.r = r_;
 }
